5f21d09c08a531d8ff499c2b1141176bd1d766e5
diego <diegopgcds10@gmail.com>
2022-01-13 14:44:30 -0300

    Configurando Redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Interval from './components/Interval';
@@ -8,23 +8,19 @@ import Sorteio from './components/Sorteio';
 
 function App() {
 
-  const [min, setMin] = useState(50)
-  const [max, setMax] = useState(100)
-
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className='linha'>
 
-      <Interval min={min} max={max}
-        onMinChanged={setMin} onMaxChanged={setMax}></Interval>
+      <Interval></Interval>
 
       </div>
       <div className='linha'>
 
-      <Media min={min} max={max}></Media>
-      <Soma min={min} max={max}></Soma>
-      <Sorteio min={min} max={max}></Sorteio>
+      <Media></Media>
+      <Soma></Soma>
+      <Sorteio></Sorteio>
 
       </div>
     </div>
--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -4,18 +4,18 @@ import Card from './Card'
 
 export default props => {
 
-    const { min, max } = props
-
-    const Random = parseInt(Math.random() * (max - min)) + min
-
     return (
         <Card title="Sorteio de um Número" purple>
             <div>
                 <span>
                     <span>Resultado:</span>
-                    <strong>{Random}</strong>
+                    <strong>{10}</strong>
                 </span>
             </div>
         </Card>
     )
 }
+
+
+
+// const Random = parseInt(Math.random() * (max - min)) + min
