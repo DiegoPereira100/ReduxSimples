722b6298598278e4d5dee543aedbe433234c9ec6
diego <diegopgcds10@gmail.com>
2022-01-13 17:26:51 -0300

    Refatorando Reducer

---
+++ src/store/reducers/numbersReducers.js
@@ -0,0 +1,23 @@
+const initialState = {
+    min: 1,
+    max: 10
+}
+
+export default function(state = initialState, action) {
+       
+    switch(action.type) {
+        case 'num_min_changed':
+            return {
+                ...state,
+                min: action.payload
+            }
+        case 'num_max_changed':
+            return {
+                ...state,
+                max: action.payload
+            }
+        default:
+            return state
+    }
+}
+
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,26 +1,9 @@
 import { createStore, combineReducers } from 'redux'
 
+import numbersReducer from './reducers/numbersReducers'
+
 const reducers = combineReducers({
-    numbers: function(state, action) {
-       
-        switch(action.type) {
-            case 'num_min_changed':
-                return {
-                    ...state,
-                    min: action.payload
-                }
-            case 'num_max_changed':
-                return {
-                    ...state,
-                    max: action.payload
-                }
-            default:
-                return {
-                    min: 5,
-                    max: 10
-                }
-        }
-    },
+    numbers: numbersReducer
     
 })
 
