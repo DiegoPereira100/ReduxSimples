cc3be3a721f3ca9b97ec15bd870007e725fe338e
diego <diegopgcds10@gmail.com>
2022-01-13 14:19:59 -0300

    Implementação sem Redux

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Interval from './components/Interval';
@@ -6,19 +7,24 @@ import Soma from './components/Soma';
 import Sorteio from './components/Sorteio';
 
 function App() {
+
+  const [min, setMin] = useState(50)
+  const [max, setMax] = useState(100)
+
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className='linha'>
 
-      <Interval></Interval>
+      <Interval min={min} max={max}
+        onMinChanged={setMin} onMaxChanged={setMax}></Interval>
 
       </div>
       <div className='linha'>
 
-      <Media></Media>
-      <Soma></Soma>
-      <Sorteio></Sorteio>
+      <Media min={min} max={max}></Media>
+      <Soma min={min} max={max}></Soma>
+      <Sorteio min={min} max={max}></Sorteio>
 
       </div>
     </div>
--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -3,12 +3,17 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+
+    const { min, max } = props
+
+    const Random = parseInt(Math.random() * (max - min)) + min
+
     return (
         <Card title="Sorteio de um Número" purple>
             <div>
                 <span>
                     <span>Resultado:</span>
-                    <strong>5</strong>
+                    <strong>{Random}</strong>
                 </span>
             </div>
         </Card>
